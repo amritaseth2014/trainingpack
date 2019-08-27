c9900656adc8630c7ec74c4f68c04d703afd69b9
Amrita Seth <Amritas@nihilentanalytics.com>
2019-08-27 15:47:10 +0530

    total marks

--- actions/roi.py
+++ actions/roi.py
@@ -3,12 +3,15 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
 
-    def run(self, p,r,t):
+    def run(self, p,r,t,math,science,geography):
 
         I = (p*r*t)/100
         A = p+I
+        total = (math+science+geography)
+        
         
 
         print("Interest of principle of {} , Rate of Interest {} and for Time Period{}  is {}".format(p,r,t, I))
         print("Total Amount is {}".format(A))
-        return(True,I,A)
+        print("Total Marks in Final Exam is {}.format"(total))
+        return(True,total)
