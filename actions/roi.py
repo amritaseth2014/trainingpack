1498636102af4f20ef4200d809d8a8eecaa70d81
Amrita Seth <Amritas@nihilentanalytics.com>
2019-08-27 16:02:10 +0530

    total marks

--- actions/roi.py
+++ actions/roi.py
@@ -3,10 +3,8 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
 
-    def run(self, p,r,t,math,science,geography):
+    def run(self,math,science,geography):
 
-        I = (p*r*t)/100
-        A = p+I
         total = (math+science+geography)
         
         print("Total Marks in Final Exam is {}".format(total))
